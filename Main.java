9e86dbcffe66203fc7fe2979d442c4a6767115db
owokaiser <101186844+owokaiser@users.noreply.github.com>
2022-03-22 12:20:38 +0000

    final commit

--- Main.java
+++ Main.java
@@ -6,10 +6,10 @@ class Main {
     double a = scana.nextDouble();
     System.out.println("Podaj B: ");
     Scanner scanb = new Scanner(System.in);
-    double b = scana.nextDouble();
+    double b = scanb.nextDouble();
     System.out.println("Podaj liczbe od 1 do 4: ");
     Scanner scany = new Scanner(System.in);
-    int y = scana.nextInt();    
+    int y = scany.nextInt();    
     double x;    
     switch(y){
       case 1:
@@ -25,8 +25,37 @@ class Main {
         System.out.println("liczba wieksza pomiedzy "+a+ " i "+b+" to: "+x);
         break;
         case 4:
-        x=a/b;
-        System.out.println("wynik dzielenia "+a+ " przez "+b+" to: "+x);
+        System.out.println("1-dodawanie\n2-odejmowanie\n3-mnozenie\n4-dzielenie ");
+        Scanner scanz = new Scanner(System.in);
+        int z = scanz.nextInt();
+        switch(z){
+          case 1:
+            x=a+b;
+            System.out.println(x);
+            break;
+            case 2:
+            x=a-b;
+            System.out.println(x);
+            break;
+            case 3:
+            x=a*b;
+            System.out.println(x);
+            break;
+            case 4:
+            if(b!=0){
+              x=a+b;
+            System.out.println(x);}
+            else{
+              System.out.println("nie dzielimy przez 0");
+            }
+            break;
+            default:
+            System.out.println("blad");
+            break;
+        }
+        break;
+        default:
+        System.out.println("blad");
         break;
     }
   }
